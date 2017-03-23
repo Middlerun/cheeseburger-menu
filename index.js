fc19424bb6a4ed896f35090398cba690e60f74a3
Eddie McLean <middlerun@gmail.com>
2017-03-23 23:18:55 +1100

    Add ES2015 classes transform

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ Object.defineProperty(exports, "__esModule", {
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
@@ -18,91 +20,112 @@ var _styles = require('./styles');
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-class CheeseburgerMenu extends _react.Component {
-  constructor() {
-    super();
-    this.state = {
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+let CheeseburgerMenu = function (_Component) {
+  _inherits(CheeseburgerMenu, _Component);
+
+  function CheeseburgerMenu() {
+    _classCallCheck(this, CheeseburgerMenu);
+
+    var _this = _possibleConstructorReturn(this, (CheeseburgerMenu.__proto__ || Object.getPrototypeOf(CheeseburgerMenu)).call(this));
+
+    _this.state = {
       swiping: false,
       swipePosition: { x: 0, y: 0 },
       menuExtraStyle: {}
     };
+    return _this;
   }
 
-  onSwipeStart(event) {
-    if (this.props.isOpen) {
+  _createClass(CheeseburgerMenu, [{
+    key: 'onSwipeStart',
+    value: function onSwipeStart(event) {
+      if (this.props.isOpen) {
+        this.setState({
+          swiping: true
+        });
+      }
+    }
+  }, {
+    key: 'onSwipeMove',
+    value: function onSwipeMove(position, event) {
+      if (this.state.swiping) {
+        const snapOpenThreshold = this.options.width / 15;
+        const pastThreshold = !this.props.right && position.x < -snapOpenThreshold || this.props.right && position.x > snapOpenThreshold;
+        const translateX = pastThreshold ? position.x : 0;
+
+        this.setState(_extends({}, this.state, {
+          swipePosition: position,
+          menuExtraStyle: {
+            transform: `translate3d(${translateX}px, 0px, 0px)`,
+            transition: 'transform 0s'
+          }
+        }));
+      }
+    }
+  }, {
+    key: 'onSwipeEnd',
+    value: function onSwipeEnd(event) {
+      const swipeCloseThreshold = this.options.width / 3;
+      if (!this.props.right && this.state.swipePosition.x < -swipeCloseThreshold || this.props.right && this.state.swipePosition.x > swipeCloseThreshold) {
+        this.props.closeCallback();
+      }
       this.setState({
-        swiping: true
+        swiping: false,
+        swipePosition: { x: 0, y: 0 },
+        menuExtraStyle: {}
       });
     }
-  }
-
-  onSwipeMove(position, event) {
-    if (this.state.swiping) {
-      const snapOpenThreshold = this.options.width / 15;
-      const pastThreshold = !this.props.right && position.x < -snapOpenThreshold || this.props.right && position.x > snapOpenThreshold;
-      const translateX = pastThreshold ? position.x : 0;
-
-      this.setState(_extends({}, this.state, {
-        swipePosition: position,
-        menuExtraStyle: {
-          transform: `translate3d(${translateX}px, 0px, 0px)`,
-          transition: 'transform 0s'
-        }
-      }));
+  }, {
+    key: 'componentWillMount',
+    value: function componentWillMount() {
+      this.options = {
+        isLeft: !this.props.right,
+        transitionTime: this.props.transitionTime || 0.3,
+        topOffset: this.props.topOffset || 0,
+        width: this.props.width || 300,
+        backgroundColor: this.props.backgroundColor || 'white',
+        showShadow: !this.props.noShadow
+      };
     }
-  }
+  }, {
+    key: 'render',
+    value: function render() {
+      const baseMenuOuterStyle = this.props.isOpen ? (0, _styles.menuOuterActiveStyle)(this.options) : (0, _styles.menuOuterStyle)(this.options);
+      const currentMenuOuterStyle = _extends({}, baseMenuOuterStyle, this.state.menuExtraStyle);
 
-  onSwipeEnd(event) {
-    const swipeCloseThreshold = this.options.width / 3;
-    if (!this.props.right && this.state.swipePosition.x < -swipeCloseThreshold || this.props.right && this.state.swipePosition.x > swipeCloseThreshold) {
-      this.props.closeCallback();
-    }
-    this.setState({
-      swiping: false,
-      swipePosition: { x: 0, y: 0 },
-      menuExtraStyle: {}
-    });
-  }
-
-  componentWillMount() {
-    this.options = {
-      isLeft: !this.props.right,
-      transitionTime: this.props.transitionTime || 0.3,
-      topOffset: this.props.topOffset || 0,
-      width: this.props.width || 300,
-      backgroundColor: this.props.backgroundColor || 'white',
-      showShadow: !this.props.noShadow
-    };
-  }
-
-  render() {
-    const baseMenuOuterStyle = this.props.isOpen ? (0, _styles.menuOuterActiveStyle)(this.options) : (0, _styles.menuOuterStyle)(this.options);
-    const currentMenuOuterStyle = _extends({}, baseMenuOuterStyle, this.state.menuExtraStyle);
-
-    return _react2.default.createElement(
-      'div',
-      { className: "cheeseburger-menu" + (this.props.isOpen ? " open" : "") },
-      _react2.default.createElement('div', { className: 'cheeseburger-menu-overlay',
-        style: this.props.isOpen ? (0, _styles.overlayActiveStyle)(this.options) : (0, _styles.overlayStyle)(this.options),
-        onClick: this.props.closeCallback }),
-      _react2.default.createElement(
-        _reactEasySwipe2.default,
-        { onSwipeStart: this.onSwipeStart.bind(this),
-          onSwipeMove: this.onSwipeMove.bind(this),
-          onSwipeEnd: this.onSwipeEnd.bind(this) },
+      return _react2.default.createElement(
+        'div',
+        { className: "cheeseburger-menu" + (this.props.isOpen ? " open" : "") },
+        _react2.default.createElement('div', { className: 'cheeseburger-menu-overlay',
+          style: this.props.isOpen ? (0, _styles.overlayActiveStyle)(this.options) : (0, _styles.overlayStyle)(this.options),
+          onClick: this.props.closeCallback }),
         _react2.default.createElement(
-          'div',
-          { className: 'cheeseburger-menu-outer', style: currentMenuOuterStyle },
+          _reactEasySwipe2.default,
+          { onSwipeStart: this.onSwipeStart.bind(this),
+            onSwipeMove: this.onSwipeMove.bind(this),
+            onSwipeEnd: this.onSwipeEnd.bind(this) },
           _react2.default.createElement(
             'div',
-            { className: 'cheeseburger-menu-inner', style: (0, _styles.menuInnerStyle)(this.options) },
-            this.props.children
+            { className: 'cheeseburger-menu-outer', style: currentMenuOuterStyle },
+            _react2.default.createElement(
+              'div',
+              { className: 'cheeseburger-menu-inner', style: (0, _styles.menuInnerStyle)(this.options) },
+              this.props.children
+            )
           )
         )
-      )
-    );
-  }
-}
+      );
+    }
+  }]);
+
+  return CheeseburgerMenu;
+}(_react.Component);
 
 CheeseburgerMenu.PropTypes = {
   isOpen: _react2.default.PropTypes.bool.isRequired,
